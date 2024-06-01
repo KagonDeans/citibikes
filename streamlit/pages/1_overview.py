107c4f2c15dc3f76883a2c04338d1eb5bb7c7010
KagonDeans <kdeans0121@gmail.com>
2024-06-01 10:30:58 -0400

    a graph

--- streamlit/pages/1_overview.py
+++ streamlit/pages/1_overview.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 st.write("#overview")
 
+cb = pd.read_csv("../data/cb_stream.csv")
+#st.write(cb)
 
-
-st.write("The data spans over 3 years, from 2019 to 2021")
+st.image("../photos/Total_trips.png")
 st.write("In total there were 67,720,005 bikes rides")
+
+st.bar_chart(data=cb, x='year', y='num_of_trips', color=None, width=None, height=None, use_container_width=True)
+
