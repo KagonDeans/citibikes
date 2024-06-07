89680f4c00754224676b5e09d74f89cfd05bfa12
KagonDeans <kdeans0121@gmail.com>
2024-06-06 23:02:07 -0400

    Great foundation for first 3 pages

--- streamlit/pages/2_dashboard.py
+++ streamlit/pages/2_dashboard.py
@@ -7,43 +7,93 @@ import statsmodels.formula.api as smf
 
 cb = pd.read_csv("../data/citibike_vis.csv")
 
-st.write("# Coef")
+lm_med_len = smf.ols('median_trip_duration ~ tmax + season + C(year) + C(month) + holiday + did_rain + is_snow + C(day_of_week) ', data = cb).fit()
 
-# # Days of week
-#     days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    
-#     # Grouping by day of the week and summing the number of trips.
-#     daily_trips = cb.groupby('day_of_week')['num_of_trips'].sum().reset_index()
+feature_name = {
+    'Week Days': [
+        ['C(day_of_week)[T.1]', 'C(day_of_week)[T.2]', 'C(day_of_week)[T.3]', 'C(day_of_week)[T.4]', 'C(day_of_week)[T.5]', 'C(day_of_week)[T.6]'],
+        ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    ],
+    'Season': [
+        ['season[T.Spring]', 'season[T.Summer]', 'season[T.Winter]'],
+        ['Spring', 'Summer', 'Winter']
+    ],
+    'Month': [
+        ['C(month)[T.2]', 'C(month)[T.3]', 'C(month)[T.4]', 'C(month)[T.5]', 'C(month)[T.6]', 'C(month)[T.7]', 'C(month)[T.8]', 'C(month)[T.9]', 'C(month)[T.10]', 'C(month)[T.11]', 'C(month)[T.12]'],
+        ['Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+    ]
+}
 
-#     # Plotting 
-#     fig = px.bar(daily_trips, x = 'day_of_week', y = 'num_of_trips', 
-#                     title = 'Total Number of Trips per Day of the Week', 
-#                     labels = {'day_of_week': 'Day of the Week', 'num_of_trips': 'Total Number of Trips'},
-#                     category_orders = {'day_of_week': days})
+feature = st.selectbox(
+    "Select a Predictor",
+    feature_name.keys()
+)
 
-#     # Update tick labels
-#     fig.update_layout(xaxis = {'tickvals':  daily_trips['day_of_week'], 'ticktext': days})
-    
-#     # Showing
-#     st.plotly_chart(fig)
+predictors = feature_name[feature][0]
+labels = feature_name[feature][1]
+
+coef = lm_med_len.params
+coeffs = coef[predictors]
+coeffs_df = pd.DataFrame({
+    'Predictor': labels,
+    'Coefficient': coeffs.values
+})
+
+fig = px.bar(coeffs_df, x='Predictor', y='Coefficient', 
+             title = 'Coefficients of Predictors',
+             labels = {'Predictor': 'Predictor', 'Coefficient': 'Coefficient in Seconds'})
+
+st.plotly_chart(fig)
 
 
-lm = smf.ols('num_of_trips ~ + season + day_of_week + tmax  + holiday + did_rain + awnd + rhav + rhmx + rhmn + tavg + event_happened', data = cb).fit()
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-#Graphing day of the week coefficients when controling for season, max temp, a holiday, & rain
-predictors = ['day_of_week[T.1]', 'day_of_week[T.2]', 'day_of_week[T.3]', 'day_of_week[T.4]', 'day_of_week[T.5]', 'day_of_week[T.6]']
+
+lm = smf.ols('num_of_trips ~ + season + C(day_of_week) + tmax  + holiday + did_rain + awnd + rhav + rhmx + rhmn + tavg + event_happened + C(month)', data = cb).fit()
+
+feature_name = {
+    'Week Days': [
+        ['C(day_of_week)[T.1]', 'C(day_of_week)[T.2]', 'C(day_of_week)[T.3]', 'C(day_of_week)[T.4]', 'C(day_of_week)[T.5]', 'C(day_of_week)[T.6]'],
+        ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    ],
+    'Season': [
+        ['season[T.Spring]', 'season[T.Summer]', 'season[T.Winter]'],
+        ['Spring', 'Summer', 'Winter']
+    ],
+    'Month': [
+        ['C(month)[T.2]', 'C(month)[T.3]', 'C(month)[T.4]', 'C(month)[T.5]', 'C(month)[T.6]', 'C(month)[T.7]', 'C(month)[T.8]', 'C(month)[T.9]', 'C(month)[T.10]', 'C(month)[T.11]', 'C(month)[T.12]'],
+        ['Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+    ]
+}
+
+feature_trips = st.selectbox(
+    "Select a Predictor_2",
+    feature_name.keys()
+)
+
+predictors = feature_name[feature_trips][0]
+labels = feature_name[feature_trips][1]
 
 coef = lm.params
 coeffs = coef[predictors]
 coeffs_df = pd.DataFrame({
-    'Predictor': ['Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    'Predictor': labels,
     'Coefficient': coeffs.values
 })
 
-
 fig = px.bar(coeffs_df, x='Predictor', y='Coefficient', 
-             title='Coefficients of Predictors',
-             labels={'Predictor': 'Day of the Week', 'Coefficient': 'Coefficient'})
+             title = 'Coefficients of Predictors',
+             labels = {'Predictor': 'Predictor', 'Coefficient': 'Coefficient of trips per by'})
+
+st.plotly_chart(fig)
+
+
+
+
+
+
+
+
 
 
 
